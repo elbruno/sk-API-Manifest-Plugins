3bea4862a2afc155b17045167961ae8db73d1549
El Bruno <elbruno@users.noreply.github.com>
2024-05-06 23:48:53 +0000

    test aspire launch

--- src/ApiServices.AppHost/Program.cs
+++ src/ApiServices.AppHost/Program.cs
@@ -1,12 +1,19 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+var apiServicePets = builder.AddProject<PetStoreAPI>("apiservicepets")
+    .WithHttpEndpoint(port: 8100) ;
+var apiServiceStore = builder.AddProject<SuperHeroesAPI>("apiservicesuperheroes")
+    .WithHttpEndpoint(port: 8188) ;
 
-builder.AddProject<Projects.AspireSample_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(cache)
-    .WithReference(apiService);
+// builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+//     .WithExternalHttpEndpoints()
+//     .WithReference(cache)
+//     .WithReference(apiService);
+
 
 builder.Build().Run();
+
