7f9a5605dac67c636dfccce9a53471f7a2393068
El Bruno <elbruno@users.noreply.github.com>
2024-05-07 15:32:44 +0000

    fis folder names

--- src/ApiServices.AppHost/Program.cs
+++ src/ApiServices.AppHost/Program.cs
@@ -1,19 +1,11 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+// var apiServicePets = builder.AddProject<Projects.PetStoreAPI>("apiservicepets")
+//     .WithHttpEndpoint(port: 8100) ;
+// var apiServiceStore = builder.AddProject<Projects.SuperHeroesAPI>("apiservicesuperheroes")
+//     .WithHttpEndpoint(port: 8188) ;
 
-var apiServicePets = builder.AddProject<PetStoreAPI>("apiservicepets")
-    .WithHttpEndpoint(port: 8100) ;
-var apiServiceStore = builder.AddProject<SuperHeroesAPI>("apiservicesuperheroes")
-    .WithHttpEndpoint(port: 8188) ;
-
-// builder.AddProject<Projects.AspireSample_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(cache)
-//     .WithReference(apiService);
-
+builder.AddProject<Projects.skTestApis>("sktestpApis");
 
 builder.Build().Run();
 
