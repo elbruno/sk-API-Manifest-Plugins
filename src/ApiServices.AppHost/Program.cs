f581704b08dc4561a8a7f882ffddc8a79e1554ee
El Bruno <elbruno@users.noreply.github.com>
2024-05-07 16:08:58 +0000

    include tests to see if console works in Aspire

--- src/ApiServices.AppHost/Program.cs
+++ src/ApiServices.AppHost/Program.cs
@@ -1,11 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// var apiServicePets = builder.AddProject<Projects.PetStoreAPI>("apiservicepets")
-//     .WithHttpEndpoint(port: 8100) ;
-// var apiServiceStore = builder.AddProject<Projects.SuperHeroesAPI>("apiservicesuperheroes")
-//     .WithHttpEndpoint(port: 8188) ;
-
 builder.AddProject<Projects.skTestApis>("sktestpApis");
+
+var apiServicePets = builder.AddProject<Projects.PetStoreAPI>("apiservicepets")
+    .WithHttpEndpoint(port: 8100) ;
+var apiServiceStore = builder.AddProject<Projects.SuperHeroesAPI>("apiservicesuperheroes")
+    .WithHttpEndpoint(port: 8188) ;
 
 builder.Build().Run();
 
